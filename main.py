520064cb7c22cc13b0e964dd99d152e674abfce0
ntaco <67748744+ntacoffee@users.noreply.github.com>
2020-08-03 23:01:25 +0900

    first commit

---
+++ main.py
@@ -0,0 +1,41 @@
+from math import e
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+def main():
+    # func = sigmoid
+    # func = tanh
+    func = relu
+
+    x = np.linspace(-5, 5, 100)
+    y, y_prime = func(x)
+
+    plt.plot(x, y)
+    plt.show()
+
+    plt.plot(x, y_prime)
+    plt.show()
+
+
+def sigmoid(x):
+    fx = 1.0 / (1.0 + e ** (-x))
+    dfdx = (1.0 - fx) * fx
+    return fx, dfdx
+
+
+def tanh(x):
+    fx = (e ** x - e ** (-x)) / (e ** x + e ** (-x))
+    dfdx = 4.0 / (e ** x + e ** (-x)) ** 2
+    return fx, dfdx
+
+
+def relu(x):
+    fx = x * (x > 0)
+    dfdx = 1 * (x > 0)
+    return fx, dfdx
+
+
+if __name__ == "__main__":
+    main()
