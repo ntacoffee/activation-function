3ae5b543f8fad6fa4611f6f913682e906bbd70d2
nTaco <67748744+ntacoffee@users.noreply.github.com>
2020-09-15 22:29:20 +0900

    softplusを追加

--- main.py
+++ main.py
@@ -7,7 +7,8 @@ import numpy as np
 def main():
     # func = sigmoid
     # func = tanh
-    func = relu
+    # func = relu
+    func = softplus
 
     x = np.linspace(-5, 5, 100)
     y, y_prime = func(x)
@@ -34,6 +35,12 @@ def tanh(x):
 def relu(x):
     fx = x * (x > 0)
     dfdx = 1 * (x > 0)
+    return fx, dfdx
+
+
+def softplus(x):
+    fx = np.log(1 + np.exp(x))
+    dfdx = 1.0 / (1.0 + np.exp(-x))
     return fx, dfdx
 
 
